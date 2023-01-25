afd7f73615786b8cf7a14371704a3a7861e87e2e
leeduu <lidiya_by@mail.ru>
2023-01-25 21:05:55 +0300

    6

--- src/main/java/web/dao/CarsDaoImp.java
+++ src/main/java/web/dao/CarsDaoImp.java
@@ -1,13 +1,13 @@
 package web.dao;
 
-import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Repository;
 import web.models.Car;
 
 import java.util.ArrayList;
 import java.util.List;
 import java.util.stream.Collectors;
 
-@Component
+@Repository
 public class CarsDaoImp implements CarsDao {
 
     private ArrayList<Car> cars;
--- src/main/java/web/service/CarsServiceImp.java
+++ src/main/java/web/service/CarsServiceImp.java
@@ -1,15 +1,12 @@
 package web.service;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Repository;
 import org.springframework.stereotype.Service;
 import web.dao.CarsDao;
-import web.dao.CarsDaoImp;
 import web.models.Car;
 
 import java.util.List;
 
-@Repository
+@Service
 public class CarsServiceImp implements CarsService {
 
     final CarsDao carsDao;
